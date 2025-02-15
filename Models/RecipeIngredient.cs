7f5d67626b6ed51fa27d3991f2b35e53f57bd90b
shhelll <155380527+shhelll@users.noreply.github.com>
2025-02-15 17:29:08 +0800

    fixed circular reference error and added dish to recipe creation

--- Models/RecipeIngredient.cs
+++ Models/RecipeIngredient.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 
 namespace Cold_Storage_GO.Models
 {
@@ -22,6 +24,7 @@ namespace Cold_Storage_GO.Models
         public Guid RecipeId { get; set; }
 
         [ForeignKey("RecipeId")]
+        [JsonIgnore]
         public Recipe? Recipe { get; set; }
     }
 }
--- Models/RecipeInstruction.cs
+++ Models/RecipeInstruction.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 
 namespace Cold_Storage_GO.Models
 {
@@ -19,6 +21,7 @@ namespace Cold_Storage_GO.Models
         public Guid RecipeId { get; set; }
 
         [ForeignKey("RecipeId")]
+        [JsonIgnore]
         public Recipe? Recipe { get; set; }
     }
 }
